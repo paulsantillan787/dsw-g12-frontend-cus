de06a17a764231417d5decc2c198084f6ece16eb
paulsantillan787 <paulggsch@gmail.com>
2024-06-12 16:33:34 -0500

    Register Espcialista

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,6 +9,8 @@ import { NgxPaginationModule } from 'ngx-pagination';
 import { CommonModule } from '@angular/common';
 import { Router, ActivatedRoute} from '@angular/router';
 import { NgbModal, NgbModalConfig, NgbModule } from '@ng-bootstrap/ng-bootstrap';  // npm install @ng-bootstrap/ng-bootstrap
+import { Especialista } from '../models/especialista';
+import { EspecialistaService } from '../services/especialista.service';
 
 
 @Component({
@@ -20,7 +22,8 @@ import { NgbModal, NgbModalConfig, NgbModule } from '@ng-bootstrap/ng-bootstrap'
 })
 export class LoginComponent {
   loginForm: FormGroup; // Use the non-null assertion operator here
-  registerForm: FormGroup;
+  registerEstudianteForm: FormGroup;
+  registerEspecialistaForm: FormGroup;
   isFormSubmitted: boolean = false;
   usuarios: Usuario[] = [];
   user: String = '';
@@ -29,6 +32,7 @@ export class LoginComponent {
     @Inject(LOCALE_ID) public locale: string,
     private usuariosService: UsuariosService,
     private estudianteService: EstudianteService,
+    private especialistaService: EspecialistaService,
     private router: Router,
     private modalService: NgbModal,
     private route: ActivatedRoute
@@ -48,7 +52,7 @@ export class LoginComponent {
       password: new FormControl('', [Validators.required])
     });
 
-    this.registerForm = new FormGroup({
+    this.registerEstudianteForm = new FormGroup({
       //Modelo Usuario
       tipo_documento: new FormControl('', [Validators.required]),
       documento: new FormControl('', [Validators.required]),
@@ -59,6 +63,7 @@ export class LoginComponent {
       correo: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required]),
       confirm_password: new FormControl('', [Validators.required]),
+
       //Modelo Estudiante
       anio_ingreso: new FormControl('', [Validators.required]),
       base: new FormControl('', [Validators.required]),
@@ -66,6 +71,23 @@ export class LoginComponent {
       ciclo_actual: new FormControl('', [Validators.required]),
       cod_estudiante: new FormControl('', [Validators.required]),
       facultad: new FormControl('', [Validators.required])
+    });
+
+    this.registerEspecialistaForm = new FormGroup({
+      //Modelo Usuario
+      tipo_documento: new FormControl('', [Validators.required]),
+      documento: new FormControl('', [Validators.required]),
+      nombre: new FormControl('', [Validators.required]),
+      apellido_paterno: new FormControl('', [Validators.required]),
+      apellido_materno: new FormControl('', [Validators.required]),
+      telefono: new FormControl('', [Validators.required]),
+      correo: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required]),
+      confirm_password: new FormControl('', [Validators.required]),
+
+      //Modelo Especialista
+      id_especialista: new FormControl('', []),
+      numero_de_colegiatura: new FormControl('', [Validators.required]),
     });
   }
 
@@ -110,44 +132,116 @@ export class LoginComponent {
   }
 
   register() {
-    if (this.registerForm.valid) {
-      //JSON para el modelo Usuario
-      const usuario: Usuario = {
-        tipo_documento: this.registerForm.value.tipo_documento,
-        documento: this.registerForm.value.documento,
-        nombre: this.registerForm.value.nombre,
-        apellido_paterno: this.registerForm.value.apellido_paterno,
-        apellido_materno: this.registerForm.value.apellido_materno,
-        telefono: this.registerForm.value.telefono,
-        correo: this.registerForm.value.correo,
-        password: this.registerForm.value.password,
-        id_tipo_rol: 1
-      };
-      this.usuariosService.insert(usuario).subscribe(
-        (response: any) => {
-          console.log(response.message);
-          //JSON para el modelo Estudiante
-          const estudiante: Estudiante = {
-            anio_ingreso: this.registerForm.value.anio_ingreso,
-            base: this.registerForm.value.base,
-            carrera: this.registerForm.value.carrera,
-            ciclo_estudio: this.registerForm.value.ciclo_actual,
-            cod_alumno: this.registerForm.value.cod_estudiante,
-            facultad: this.registerForm.value.facultad,
-            documento: this.registerForm.value.documento
-          };
+    if (this.registerEstudianteForm.valid) {
 
-          this.estudianteService.insert(estudiante).subscribe(
+      if (this.user === 'estudiante') {
+      //JSON para el modelo Usuario
+        const usuario: Usuario = {
+          tipo_documento: this.registerEstudianteForm.value.tipo_documento,
+          documento: this.registerEstudianteForm.value.documento,
+          nombre: this.registerEstudianteForm.value.nombre,
+          apellido_paterno: this.registerEstudianteForm.value.apellido_paterno,
+          apellido_materno: this.registerEstudianteForm.value.apellido_materno,
+          telefono: this.registerEstudianteForm.value.telefono,
+          correo: this.registerEstudianteForm.value.correo,
+          password: this.registerEstudianteForm.value.password,
+          id_tipo_rol: 1
+        };
+        this.usuariosService.insert(usuario).subscribe(
+          (response: any) => {
+            console.log(response.message);
+            //JSON para el modelo Estudiante
+            const estudiante: Estudiante = {
+              anio_ingreso: this.registerEstudianteForm.value.anio_ingreso,
+              base: this.registerEstudianteForm.value.base,
+              carrera: this.registerEstudianteForm.value.carrera,
+              ciclo_estudio: this.registerEstudianteForm.value.ciclo_actual,
+              cod_alumno: this.registerEstudianteForm.value.cod_estudiante,
+              facultad: this.registerEstudianteForm.value.facultad,
+              documento: this.registerEstudianteForm.value.documento
+            };
+
+            this.estudianteService.insert(estudiante).subscribe(
+              (response: any) => {
+                console.log(response.message);
+                Swal.fire({
+                  title: 'Registro exitoso',
+                  text: response.message,
+                  icon: 'success',
+                  confirmButtonText: 'Aceptar'
+                });
+                this.registerEstudianteForm.reset();
+                this.modalService.dismissAll();
+              },
+              error => {
+                console.log(error);
+                Swal.fire({
+                  title: 'Error',
+                  text: error.message,
+                  icon: 'error',
+                  confirmButtonText: 'Aceptar'
+                });
+              }
+            );
+          },
+          error => {
+            console.log(error);
+            Swal.fire({
+              title: 'Error',
+              text: error.message,
+              icon: 'error',
+              confirmButtonText: 'Aceptar'
+            });
+          }
+        );
+      }
+
+    } else if (this.registerEspecialistaForm.valid) {
+        if (this.user === 'especialista') {
+        //JSON para el modelo Usuario
+          const usuario: Usuario = {
+            tipo_documento: this.registerEspecialistaForm.value.tipo_documento,
+            documento: this.registerEspecialistaForm.value.documento,
+            nombre: this.registerEspecialistaForm.value.nombre,
+            apellido_paterno: this.registerEspecialistaForm.value.apellido_paterno,
+            apellido_materno: this.registerEspecialistaForm.value.apellido_materno,
+            telefono: this.registerEspecialistaForm.value.telefono,
+            correo: this.registerEspecialistaForm.value.correo,
+            password: this.registerEspecialistaForm.value.password,
+            id_tipo_rol: 2
+          };
+          this.usuariosService.insert(usuario).subscribe(
             (response: any) => {
               console.log(response.message);
-              Swal.fire({
-                title: 'Registro exitoso',
-                text: response.message,
-                icon: 'success',
-                confirmButtonText: 'Aceptar'
-              });
-              this.registerForm.reset();
-              this.modalService.dismissAll();
+              //JSON para el modelo Especialista
+              const especialista: Especialista = {
+                id_especialista: 0,
+                numero_de_colegiatura: this.registerEspecialistaForm.value.numero_de_colegiatura,
+                documento: this.registerEspecialistaForm.value.documento
+              };
+
+              this.especialistaService.insert(especialista).subscribe(
+                (response: any) => {
+                  console.log(response.message);
+                  Swal.fire({
+                    title: 'Registro exitoso',
+                    text: response.message,
+                    icon: 'success',
+                    confirmButtonText: 'Aceptar'
+                  });
+                  this.registerEspecialistaForm.reset();
+                  this.modalService.dismissAll();
+                },
+                error => {
+                  console.log(error);
+                  Swal.fire({
+                    title: 'Error',
+                    text: error.message,
+                    icon: 'error',
+                    confirmButtonText: 'Aceptar'
+                  });
+                }
+              );
             },
             error => {
               console.log(error);
@@ -159,20 +253,8 @@ export class LoginComponent {
               });
             }
           );
-        },
-        error => {
-          console.log(error);
-          Swal.fire({
-            title: 'Error',
-            text: error.message,
-            icon: 'error',
-            confirmButtonText: 'Aceptar'
-          });
         }
-      );
-
-
-    }
+      }
   }
 
 }
