c6d0865266d75e8fbcf0e84bb47270f4c6f1c2ef
AbelLaptop <abel07110711@gmail.com>
2024-06-19 22:19:06 -0500

    home added

--- src/app/sites/home/home.component.ts
+++ src/app/sites/home/home.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NgbModal, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NgbModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  user: String | null = "";
+  constructor(
+    private modalService: NgbModal,
+  ) {
+    this.user = localStorage.getItem('user');
+
+    console.log("User: ", this.user);
+}
 
 }
